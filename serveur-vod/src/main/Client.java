84b8533b9a437a3485282be9fa314c7e3012b380
LoicLeBris <loic.le-bris@etu.univ-cotedazur.fr>
2022-10-03 10:48:15 +0200

    Add connection

--- serveur-vod/src/main/Client.java
+++ serveur-vod/src/main/Client.java
@@ -1,4 +1,19 @@
 package main;
 
 public class Client {
+    String password;
+    String mail;
+
+    Client(String mail, String pwd){
+        this.password = pwd;
+        this.mail = mail;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getMail() {
+        return mail;
+    }
 }
--- serveur-vod/src/main/Connection.java
+++ serveur-vod/src/main/Connection.java
@@ -5,15 +5,81 @@ import exceptions.InvalidCredentialsException;
 import exceptions.SignUpFailed;
 
 import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Connection implements IConnection {
+    List<Client> clientList;
+
+    Connection() {
+        clientList = new ArrayList<>();
+        displayRegisterOrConnection();
+        for(Client c : clientList){
+            System.out.println(c.getMail());
+        }
+    }
+
+    private void displayRegisterOrConnection() {
+        System.out.println("Do you have an account (y/n) ?\n");
+        Scanner scanner = new Scanner(System.in);
+        String answer = scanner.next();
+        switch(answer){
+            case "y" : displayConnection();
+            case "n" : displayRegister();
+        }
+    }
+
+    private void displayRegister() {
+        System.out.println("Register :\n Enter your email : ");
+        Scanner scanner = new Scanner(System.in);
+        String email = scanner.next();
+        System.out.println("\n Enter your password : ");
+        String password = scanner.next();
+        try {
+            signUp(email,password);
+        } catch (SignUpFailed e) {
+            System.out.println("Your email seems to already be used, try again");
+            displayRegister();
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void displayConnection() {
+        System.out.println("Connection :\n Enter your email : ");
+        Scanner scanner = new Scanner(System.in);
+        String email = scanner.next();
+        System.out.println("\n Enter your password : ");
+        String password = scanner.next();
+        try {
+            logIn(email,password);
+        } catch (InvalidCredentialsException e) {
+            System.out.println("Your email seems to not be valid, try again");
+            displayConnection();
+        }
+    }
+
     @Override
     public boolean signUp(String mail, String password) throws SignUpFailed, RemoteException {
-        return false;
+        for(Client c : clientList){
+            if(c.getMail().equals(mail)){
+                throw new SignUpFailed();
+            }
+        }
+
+        Client newClient = new Client(mail, password);
+        clientList.add(newClient);
+
+        return true;
     }
 
     @Override
     public boolean logIn(String mail, String password) throws InvalidCredentialsException {
-        return false;
+        for(Client c: clientList){
+            if(c.getMail().equals(mail) && c.getPassword().equals(password)) return true;
+        }
+
+        throw new InvalidCredentialsException();
     }
 }
